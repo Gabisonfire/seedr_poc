34ac699ec2052faa69240461ebcbee30a08693dd
Gabisonfire <gab.despres@gmail.com>
2022-04-04 23:27:21 -0400

    Added threads and logical optimizations

--- seedarr.py
+++ seedarr.py
@@ -16,6 +16,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 parser = argparse.ArgumentParser()
 parser.add_argument("--add-id", help="Add a TMDBid to watch.", type=int)
 parser.add_argument("--skip-hash", help="Skip the file hash check.", action="store_true")
+parser.add_argument("--no-delete", help="Don't delete the original file.", action="store_true")
 parser.add_argument("--force-state-change", help="Skip the file hash check.", type=int)
 parser.add_argument("-c", "--config", help="Path to the config file.", type=int)
 args = parser.parse_args()
@@ -69,6 +70,10 @@ def get_missing():
 
 def update_state():
     found = False
+    unwatch = []
+    if len(settings.watch) == 0:
+        logger.info("Watch queue empty.")
+        return
     logger.info("Looking for state changes...")
     for id in settings.watch:
         movie = settings.radarr.get_movie(id)
@@ -77,45 +82,62 @@ def update_state():
             if movie['hasFile'] or movie['tmdbId'] == args.force_state_change:
                 found = True
                 logger.info(f"Movie '{movie['title']}' has changed state, queueing for torrent client check.")
-                settings.changed.append(id)
+                if id not in settings.changed:
+                    settings.changed.append(id)
+                if id not in unwatch:
+                    unwatch.append(id)
+    for i in unwatch:
+        settings.watch.remove(i)
     if not found:
         logger.info("No state changes found.")
-
 
 def blake(file):
     with open(file, "rb") as f:
         file_hash = hashlib.blake2b()
-        while chunk := f.read(8192):
+        while chunk := f.read():
             file_hash.update(chunk)
-
     return file_hash.hexdigest()
 
 def move_torrent(t, movie):
+    # Removing before hash check to prevent threads from analyzing the same file
+    if movie['tmdbId'] in settings.changed:
+        settings.changed.remove(movie['tmdbId'])
     new_path = movie['movieFile']['path'].replace(cfg.read_config("radarr_library_directory"), cfg.read_config("torrent_library_directory"))
     if not args.skip_hash:
         logger.info("Comparing hashes to ensure files are identical")
         lib_file = blake(t['content_path'])
         download_file = blake(new_path)
-        logger.info(f"{lib_file} -- {t['content_path']}")
-        logger.info(f"{download_file} -- {new_path}")
+        logger.debug(f"{lib_file} -- {t['content_path']}")
+        logger.debug(f"{download_file} -- {new_path}")
         if not lib_file == download_file:
-            logger.error("Invalid hash.")
+            logger.error("Invalid hashes, re-qeueuing.")
             return
+        else:
+            logger.info(f"{lib_file} and {download_file} hashes are identical.")
+    else:
+        logger.info("'--skip-hash' set, skipping hash check.")
     logger.info(f"Moving torrent({t['hash']}) from {t['content_path']} to {new_path}")
     try:
         og_path = t['content_path']
         t.set_location(location=os.path.dirname(new_path))
-        logger.info("Torrent moved. Removing from watch queue.")
-        settings.changed.remove(movie['tmdbId'])
-        settings.watch.remove(movie['tmdbId'])
-        logger.info("Queueing for deletion")
-        settings.to_delete.append({"torrent": t, "original_path": og_path})
+        logger.info(f"{movie['title']} moved. Removing from watch queue.")
+        if movie['tmdbId'] in settings.changed:
+            settings.changed.remove(movie['tmdbId'])
+        logger.info(f"Queueing {t['name']} for deletion")
+        if {"torrent": t, "original_path": og_path} not in settings.to_delete:
+            settings.to_delete.append({"torrent": t, "original_path": og_path})
     except Exception as e:
         logger.error(f"Could not move torrent: {t['name']}. {e}")
+        # re-adding on failure
+        if movie['tmdbId'] not in settings.changed:
+            settings.changed.append(movie['tmdbId'])
 
-def verify_torrents():
+def match_and_move_torrents():
     found = False
     torrents = settings.client.torrents_info(category=cfg.read_config("torrent_category"))
+    if len(settings.changed) == 0:
+        logger.info("Change queue empty.")
+        return
     if len(torrents) > 0:
         logger.info("Looking for torrent file match...")
     for id in settings.changed:
@@ -126,40 +148,45 @@ def verify_torrents():
             for t in torrents:
                 if ntpath.basename(t['content_path']) == movie['movieFile']['relativePath']:
                     found = True
-                    logger.info(f"Found a match with torrent: {t['name']}. Changing location")
+                    logger.info(f"Found a match with torrent: {t['name']}.")
                     move_torrent(t, movie)
     if not found and len(torrents) > 0:
-        logger.info("No matching torrent found.")
+        logger.warning(f"{len(settings.changed)} changes in Radarr but no match found in your torrent client.")
 
 def check_and_delete():
     deleted = []
     for torrent in settings.to_delete:
         status = settings.client.torrents_info(torrent_hashes=torrent["torrent"]['hash'])[0]
-        if status['state'] not in ["error", "checkingUP", "moving", "unknown"]: # User provided acceptable state
-            logger.info(f"Torrent's state is {status['state']}, deleting: {torrent['original_path']}")
-            # ACTUALLY DELETE THE FILE
-            deleted.append(torrent)
+        if status['state'] not in ["error", "checkingUP", "moving", "unknown"]: 
+            logger.info(f"{status['name']}'s state is {status['state']}, deleting: {torrent['original_path']}")
+            if not args.no_delete:
+                try:
+                    os.remove(torrent['original_path'])
+                    if torrent not in deleted:
+                        deleted.append(torrent)
+                    logger.info(f"{torrent['original_path']} deleted.")
+                except Exception as e:
+                    logger.error(f"Error deleting {torrent['original_path']}, {e}")
+            else:
+                logger.info("'--no-delete' set, skipping deletion.")
+                if torrent not in deleted:
+                    deleted.append(torrent)
         else:
-            logger.info(f"Torrent's state is {status['state']}, not ready for deletion.")
+            logger.info(f"{status['name']}'s state is {status['state']}, not ready for deletion.")
     for i in deleted:
         settings.to_delete.remove(i)
-
-def tick():
-    get_missing()
-    update_state()
-    verify_torrents()
-    check_and_delete()
 
 init()
 check_endpoints()
 logger.info("Starting scheduler...")
+sched.add_job(get_missing, 'interval', seconds=cfg.read_config("missing_status_scan_interval"), max_instances=10, id="missing_status_scan")
+sched.add_job(update_state, 'interval', seconds=cfg.read_config("state_change_scan_interval"), max_instances=10, id="state_change_scan")
+sched.add_job(match_and_move_torrents, 'interval', seconds=cfg.read_config("match_and_move_torrents_scan_interval"), max_instances=10, id="match_and_move_torrents_scan")
+sched.add_job(check_and_delete, 'interval', seconds=cfg.read_config("check_and_delete_scan_interval"), max_instances=1, id="check_and_delete_scan")
 sched.start()
-sched.add_job(tick, 'interval', seconds=cfg.read_config("scan_interval"), max_instances=1, id="main_loop")
-logger.info("Ready.")
+logger.info("Waiting.")
 while True:
     time.sleep(1)
 
-# need to delete torrent (schedule for deletion if status not checking)
+# # User provided acceptable states?
 # need save/load ids
-# 1 job per task step (seperate interval)
-# watch to changed remove right away
